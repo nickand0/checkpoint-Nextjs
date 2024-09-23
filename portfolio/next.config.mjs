4dcb7831579e025f783bf2acef9c1e9c4aef9311
nickand0 <andoniainanikki@gmail.com>
2024-09-23 00:51:20 +0000

    image error

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',  // Ajoute cette ligne pour permettre l'exportation statique
+    output: 'export',
+    images: {
+        unoptimized: true,  // Désactive les optimisations d'images pour un export statique
+      },  // Ajoute cette ligne pour permettre l'exportation statique
   };
   
   export default nextConfig;
